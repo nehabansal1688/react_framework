dcf567f935de526edce984f3b6027f58ad8b4d4a
nehabansal1688 <nehabansal1688@gmail.com>
2018-05-30 12:17:12 +0530

    counter implemenattion in file

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
-const Contact = (props) => {
-  return (
-    <div>
-      <h2>Contact</h2>
-    </div>
-  )
-}
+const Contact = React.createClass({ 
+	 incrementCouter() {
+	 	this.setState({
+	 		count: this.state.count +1
+	 	});
+	},
+	getInitialState() {
+		return {
+			count:0
+		}
+	},
+	render(){
+	  return (
+	    <div>
+	      <h2>Contact</h2>
+	      <h3>Counter value : {this.state.count}</h3>
+	       <button className ="btn-primary" type="button" onClick={this.incrementCouter}>Increment</button>
+	    </div>
+  	)}
+});
 
 export default Contact;
