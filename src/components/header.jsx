da0b8dd1840a91f35da185f4b9dcc2854d0b937e
nehabansal1688 <nehabansal1688@gmail.com>
2018-05-30 12:16:20 +0530

    localized header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import localization from '../localization/localization.jsx';
 
 class Header extends React.Component {
   renderLogo(name) {
     return <div className="brand-logo center">{name}'s React learning</div>
   }
+ 
   render(){
     return (
       <div className="navbar-fixed">
@@ -12,10 +14,10 @@ class Header extends React.Component {
         <div className="nav-wrapper">
           {this.renderLogo(this.props.name)}
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">about</Link></li>
-            <li><Link to="/contact">contact</Link></li>
-            <li><Link to="/list">List</Link></li>
+            <li><Link to="/">{localization.home}</Link></li>
+            <li><Link to="/about">{localization.about}</Link></li>
+            <li><Link to="/contact">{localization.contact}</Link></li>
+            <li><Link to="/list">{localization.list}</Link></li>
           </ul>
         </div>
       </nav>
