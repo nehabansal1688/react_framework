3b986ad6c0ffc3e3d1e9e8fc8fbff59b08460011
nehabansal1688 <nehabansal1688@gmail.com>
2018-05-30 12:15:07 +0530

    added axiox for server calls

--- App.js
+++ App.js
@@ -6,8 +6,9 @@ import Contact from './src/components/contact.jsx';
 import Header from './src/components/header.jsx';
 import List from './src/components/list.jsx';
 import $ from 'jquery';
+import axios from 'axios';
 
-class App extends React.Component { 
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -16,31 +17,39 @@ class App extends React.Component {
     }
   }
   componentDidMount() {
-    $.ajax({
+   /** 
+    //get data using jquery ajax
+   $.ajax({
       url: 'https://api.myjson.com/bins/11r8s1',
       dataType: 'json',
       success: function(parsed_json){
+        console.log( parsed_json);
         this.setState({data: parsed_json});
-
-        alert(parsed_json);
       }.bind(this)
-      });
+      });*/
+
+      
+    //get data from server using axios
+    var self = this;
+    axios.get('https://api.myjson.com/bins/11r8s1')
+    .then(function(response) {
+      self.setState({data: response.data})
+    })
   }
   render() {
     return (
       <Router>
-      <div> 
-        <img src ={require('./images/download.png')}/>
-      <Header name={this.state.name}/>  
+        <div>
+          <Header name={this.state.name}/>
 
-      <Switch> 
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/list" render={(props) => (<List data={this.state.data}/>)} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/list" render={(props) => (<List data={this.state.data}/>)} />
 
-      </Switch>      
-      </div>
+          </Switch>
+        </div>
     </Router>
 
     );
