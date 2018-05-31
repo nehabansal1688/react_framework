c64c4ab214eeb8606a1bdeccb73c9feedba576e9
nehabansal1688 <nehabansal1688@gmail.com>
2018-05-31 16:48:56 +0530

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 const Contact = React.createClass({ 
-	 incrementCouter() {
+	incrementCouter() {
+	 	this.props.dispatch({type:"INCREMENT"});
+	},
+	decrementCouter() {
+ 		this.props.dispatch({type:"DECREMENT"});
+	},
+
+	render(){
+	  return (
+	    <div className="container">
+	      <h2>Contact</h2>
+	       <button className ="btn-default" type="button" onClick={this.incrementCouter}>+</button>
+	       <span>{this.props.count}</span>
+	       <button className ="btn-default" type="button" onClick={this.decrementCouter}>-</button>
+	    </div>
+  	)}
+});
+
+const mapStateToProps = state => ({
+	count: state.count
+})
+export default connect(mapStateToProps)(Contact);
+
+/**import React from 'react';
+
+const Contact = React.createClass({ 
+	incrementCouter() {
 	 	this.setState({
 	 		count: this.state.count +1
 	 	});
 	},
+	decrementCouter() {
+ 		this.setState({
+ 			count : this.state.count -1
+ 		});
+	},
+
 	getInitialState() {
 		return {
 			count:0
@@ -17,8 +50,9 @@ const Contact = React.createClass({
 	      <h2>Contact</h2>
 	      <h3>Counter value : {this.state.count}</h3>
 	       <button className ="btn-primary" type="button" onClick={this.incrementCouter}>Increment</button>
+	       <button className ="btn-primary" type="button" onClick={this.decrementCouter}>Decrement</button>
 	    </div>
   	)}
 });
 
-export default Contact;
+export default Contact;**/
