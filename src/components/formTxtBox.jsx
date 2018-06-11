dd109da72390560b3a0bfb897a7616d9aeeae50e
nehabansal1688 <nehabansal1688@gmail.com>
2018-06-11 14:07:03 +0530

    Create formTxtBox.jsx

---
+++ src/components/formTxtBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+class FormTxtBox extends React.Component {
+	constructor(props){
+		super();
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleChange(e){
+		this.props.onValueChange(e);
+	}
+	render() {
+		return(
+			<div className="row form-group">
+  				<div className="col-sm-2">
+  					Qualification:
+  				</div>
+  				<div className="col-sm-4">
+  					<input type="text" className="form-control" name="qualification" onChange={this.handleChange} value={this.props.edu}/>
+  				</div>
+  			</div>
+		);
+	}
+}
+
+export default FormTxtBox;
