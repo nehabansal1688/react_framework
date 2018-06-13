b1db14bf879742178bc12c85156bddfa254fef84
nehabansal1688 <nehabansal1688@gmail.com>
2018-06-13 17:40:55 +0530

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import LifeCycle from './lifeCycle.jsx';
 
 const Home = (props) => {
   return (
     <div>
       <h2>Home</h2>
+      <LifeCycle data="123"/>
     </div>
   )
 }
